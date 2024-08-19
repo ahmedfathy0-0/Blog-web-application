46c5738e483a2769888fe44e88c0f6c8192eb69e
ahmedfathy0-0 <ahmedfathi20044002@gmail.com>
2024-08-19 08:30:17 +0300

    add New Post feature :fire:

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import multer from 'multer';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
+
+class Post {
+    constructor(auther, image, content) {
+        this.auther = auther;
+        this.image = image;
+        this.content = content;
+    }
+}
+
+let postvector = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/newpost', (req, res) => {
+    res.render('newpost.ejs');
+});
+
+app.post('/submit', upload.single('image'), (req, res) => {
+    const imagePath = req.file ? `/uploads/${req.file.filename}` : null;
+    const newPost = new Post(req.body.name, imagePath, req.body.blog);
+    postvector.push(newPost);
+    res.redirect('/'); // Redirect or render a response after submission
+    console.log(postvector);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
