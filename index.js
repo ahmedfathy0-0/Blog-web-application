b78f84ed094c4d56f4f8f8aea390687dfdd1ceae
ahmedfathy0-0 <ahmedfathi20044002@gmail.com>
2024-08-20 07:14:09 +0300

    delete and update feature is done :zap:

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage: storage });
+let ID = 0;
 
 class Post {
     constructor(title, image, content) {
+        this.id = ID++;
         this.title = title;
         this.image = image;
         this.content = content;
@@ -31,13 +33,12 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index.ejs', { posts: postvector });
-    
+
 });
 
 app.get('/newpost', (req, res) => {
     res.render('newpost.ejs');
 });
-
 app.post('/submit', upload.single('image'), (req, res) => {
     const imagePath = req.file ? `/uploads/${req.file.filename}` : null;
     const newPost = new Post(req.body.name, imagePath, req.body.blog);
@@ -45,6 +46,48 @@ app.post('/submit', upload.single('image'), (req, res) => {
     res.redirect('/'); // Redirect or render a response after submission
     console.log(postvector);
 });
+
+app.get('/updatepost' , (req, res) => {
+    res.render('updatepost.ejs', { posts: postvector });
+});
+app.get('/update/:id', (req, res) => {
+    const post = postvector.find((post) => post.id == req.params.id);
+    res.render('update.ejs', { post });
+});
+
+app.post('/update/:id', upload.single('image'), (req, res) => {
+    const post = postvector.find((post) => post.id == req.params.id);
+
+    if (post) {
+        post.title = req.body.name;
+        post.content = req.body.blog;
+        if (req.file) {
+            post.image = `/uploads/${req.file.filename}`;
+        } else if (req.body.oldImage) {
+            post.image = req.body.oldImage; 
+        }
+        res.redirect('/'); 
+    } else {
+        res.status(404).send('Post not found');
+    }
+});
+
+app.get('/deletepost' , (req, res) => {
+    res.render('delete.ejs', { posts: postvector });
+});
+app.get('/delete/:id', (req, res) => {
+    postvector = postvector.filter((post) => post.id != req.params.id);
+    res.redirect('/');
+});
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
