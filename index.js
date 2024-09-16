391d5c64e17e58cdc44c7efa91b893ba3f2d1d16
ahmedfathy0-0 <ahmedfathi20044002@gmail.com>
2024-09-17 00:15:23 +0300

    Refactor app configuration to include public directory

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
 
 
 const storage = multer.diskStorage({
